3cbd1363d41d4836e9b1c430f3ceaf861596aa4b
朱天行 <zhutianxing@ip-101-112.wireless.oberlin.edu>
2019-11-27 11:44:59 -0500

    webcam test

--- camera-test.py
+++ camera-test.py
@@ -6,7 +6,7 @@ cap = cv2.VideoCapture(0)
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    #cap.set(CV_CAP_PROP_FPS, 25)
+    # cap.set(CV_CAP_PROP_FPS, 25)
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
    
