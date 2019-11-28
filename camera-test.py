effa1a2291f34ea6c82f5cef8a3dc5c6a91f826a
朱天行 <zhutianxing@ip-101-112.wireless.oberlin.edu>
2019-11-27 22:28:03 -0500

    test frame

--- camera-test.py
+++ camera-test.py
@@ -1,21 +1,26 @@
 import numpy as np
 import cv2
+import time
 
 cap = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 680)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    cap.set(cv2.CAP_PROP_FPS, 30)
     ret, frame = cap.read()
     # cap.set(CV_CAP_PROP_FPS, 25)
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-   
+
     # Display the resulting frame
     cv2.imshow('frame',frame)
     cv2.imshow('gray',gray)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
+    
+    #cv2.resizeWindow('frame', 1200, 900)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
