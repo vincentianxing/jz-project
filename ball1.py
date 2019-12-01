e470955352ef48761a3da1fa48b2d2be469c65d3
unknown <liurantina@outlook.com>
2019-12-01 15:04:30 -0500

    bounce ball

--- ball1.py
+++ ball1.py
@@ -15,16 +15,9 @@ BLACK = (0,0,0)
 WIDTH = 600
 HEIGHT = 400      
 BALL_RADIUS = 20
-PAD_WIDTH = 8
-PAD_HEIGHT = 80
-HALF_PAD_WIDTH = PAD_WIDTH / 2
-HALF_PAD_HEIGHT = PAD_HEIGHT / 2
 ball_pos = [0,0]
 ball_vel = [0,0]
-#paddle_vel = 0
-#paddle2_vel = 0
 score = 0
-#r_score = 0
 
 #canvas declaration
 camera = cv2.VideoCapture(0)
@@ -32,35 +25,24 @@ pygame.init()
 pygame.display.set_caption("Play Balls!")
 screen = pygame.display.set_mode([WIDTH,HEIGHT])
 
-def ball_init(right):
+def ball_init():
     global ball_pos, ball_vel # these are vectors stored as lists
-    ball_pos = [300,200]
-    horz = int(random.randrange(2,4))
-    vert = int(random.randrange(1,3))
+    ball_pos = [300,100]
+    horz = int(random.randrange(0,4))
+    vert = 0
     
-    if right == False:
-        horz = - horz
-        
-    ball_vel = [horz,-vert]
+    ball_vel = [horz, vert]
 
 # define event handlers
 def init():
     global score  # these are floats
-    global score1, score2  # these are ints
-    #paddle1_pos = [HALF_PAD_WIDTH - 1,HEIGHT/2]
-    #paddle2_pos = [WIDTH +1 - HALF_PAD_WIDTH,HEIGHT/2]
     score = 0
-    #r_score = 0
-    if random.randrange(0,2) == 0:
-        ball_init(True)
-    else:
-        ball_init(False)
+    ball_init()
 
 def draw(canvas):
     global ball_pos, ball_vel, score
 
-    pygame.draw.line(canvas, WHITE, [0, HEIGHT + 1 - PAD_WIDTH],[WIDTH, HEIGHT + 1 - PAD_WIDTH], 1)
-
+    ball_vel[1] += 0.981
     #update ball
     ball_pos[0] += int(ball_vel[0])
     ball_pos[1] += int(ball_vel[1])
@@ -77,12 +59,19 @@ def draw(canvas):
         ball_vel[0] = -ball_vel[0]
     
     #ball collison check on gutters or paddles
-    if int(ball_pos[1]) >= HEIGHT + 1 - BALL_RADIUS - PAD_WIDTH :
-        ball_vel[0] = -ball_vel[0]
-        ball_vel[0] *= 1.1
-        ball_vel[1] *= 1.1
-        score += 1
-        ball_init(True)
+    #if int(ball_pos[1]) >= HEIGHT + 1 - BALL_RADIUS:
+    #    ball_vel[1] = -ball_vel[1]
+        #ball_vel[0] *= 1.1
+        #ball_vel[1] *= 1.1
+        #score += 1
+    #if int(ball_pos[1]) >= y + BALL_RADIUS:
+    #    ball_vel[1] = -ball_vel[1]
+
+    if int(ball_pos[1]) >= HEIGHT + 1 - BALL_RADIUS:
+        ball_vel[1] *= -1
+        if int(ball_vel[1]) == 0:
+            ball_init()
+    #    ball_init()
 
     #update scores
     myfont1 = pygame.font.SysFont("Comic Sans MS", 20)
