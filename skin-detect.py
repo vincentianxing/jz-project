21a3e5c375cd6c7c4fbf7a8fc40d361d692e9968
vincentianxing <sky103034@gmail.com>
2019-12-02 11:07:23 -0500

    skin vz

--- skin-detect.py
+++ skin-detect.py
@@ -4,7 +4,7 @@ import numpy as np
 import image_analysis
 import argparse
 
-hand_hist = None
+hand_hist = None # histogram generated from hand sample
 traverse_point = []
 total_rectangle = 9
 hand_rect_one_x = None
@@ -41,11 +41,12 @@ def max_contour(contour_list, frame):
         
     return contour_list[max_i]
 
-
+# draw region for skin sample
 def draw_rect(frame):
     rows, cols, _ = frame.shape
     global total_rectangle, hand_rect_one_x, hand_rect_one_y, hand_rect_two_x, hand_rect_two_y
 
+    # four arrays to hold the coordinates of each rectangle
     hand_rect_one_x = np.array(
         [6 * rows / 20, 6 * rows / 20, 6 * rows / 20, 9 * rows / 20, 9 * rows / 20, 9 * rows / 20, 12 * rows / 20,
          12 * rows / 20, 12 * rows / 20], dtype=np.uint32)
@@ -57,31 +58,37 @@ def draw_rect(frame):
     hand_rect_two_x = hand_rect_one_x + 10
     hand_rect_two_y = hand_rect_one_y + 10
 
+    # interates over arrays and draws rectangle on frame
     for i in range(total_rectangle):
         cv2.rectangle(frame, (hand_rect_one_y[i], hand_rect_one_x[i]),
                       (hand_rect_two_y[i], hand_rect_two_x[i]),
                       (0, 255, 0), 1)
     return frame
 
-
+# generate histogram for skin regions
 def hand_histogram(frame):
     global hand_rect_one_x, hand_rect_one_y
 
+    # transforms input frame to HSV
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # create an image of size 90 * 10 with 3 color channels
     roi = np.zeros([90, 10, 3], dtype=hsv_frame.dtype)
 
     for i in range(total_rectangle):
         roi[i * 10: i * 10 + 10, 0: 10] = hsv_frame[hand_rect_one_x[i]:hand_rect_one_x[i] + 10,
                                                     hand_rect_one_y[i]:hand_rect_one_y[i] + 10]
-
+    
+    # create a histogram using the roi matrix for the skin color
     hand_hist = cv2.calcHist([roi], [0, 1], None, [180, 256], [0, 180, 0, 256])
     return cv2.normalize(hand_hist, hand_hist, 0, 255, cv2.NORM_MINMAX)
 
-
+# find components of the frame that contains skin
+# use histogram to separate features in an image with back projection
 def hist_masking(frame, hist):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # apply back projection
     dst = cv2.calcBackProject([hsv], [0, 1], hist, [0, 180, 0, 256], 1)
-
+    # smoothen the image
     disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (33, 33))  # TODO
     cv2.filter2D(dst, -1, disc, dst)
 
@@ -171,28 +178,6 @@ def manage_image_opr(frame, hand_hist):
         draw_circles(frame, traverse_point)
 
     return frame
-
-
-"""
-def draw_final(self, frame, hand_hist):
-    hand_masked = image_analysis.apply_hist_mask(
-        frame, hand_hist)
-
-    contours = image_analysis.contours(hand_masked)
-    if contours is not None and len(contours) > 0:
-        max_contour = image_analysis.max_contour(contours)
-        hull = image_analysis.hull(max_contour)
-        centroid = image_analysis.centroid(max_contour)
-        defects = image_analysis.defects(max_contour)
-
-        if centroid is not None and defects is not None and len(defects) > 0:
-            farthest_point = image_analysis.farthest_point(
-                defects, max_contour, centroid)
-
-            if farthest_point is not None:
-                self.plot_farthest_point(frame, farthest_point)
-"""
-
 
 def main():
     # Background seperation
