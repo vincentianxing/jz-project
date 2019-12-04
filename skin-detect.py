1dff24c37fff57cc3e668ed5491b8e171c7c3b6d
vincentianxing <sky103034@gmail.com>
2019-12-04 15:11:24 -0500

    clean version

--- skin-detect.py
+++ skin-detect.py
@@ -1,17 +1,16 @@
-from __future__ import print_function
 import cv2
 import numpy as np
 import argparse
 
 hand_hist = None # histogram generated from hand sample
-size = 9
+size = 9 # number of rectangles
 hand_rect_one_x = None
 hand_rect_one_y = None
 hand_rect_two_x = None
 hand_rect_two_y = None
 traverse_point = []
 
-# resize ouput window TODO
+# resize ouput window
 def rescale_frame(frame, wpercent=90, hpercent=90):
     width = int(frame.shape[1] * wpercent / 100)
     height = int(frame.shape[0] * hpercent / 100)
@@ -26,15 +25,13 @@ def draw_rect(frame):
     hand_rect_one_x = np.array(
         [6 * rows / 20, 6 * rows / 20, 6 * rows / 20, 9 * rows / 20, 9 * rows / 20, 9 * rows / 20, 12 * rows / 20,
          12 * rows / 20, 12 * rows / 20], dtype=np.uint32)
-
     hand_rect_one_y = np.array(
         [9 * cols / 20, 10 * cols / 20, 11 * cols / 20, 9 * cols / 20, 10 * cols / 20, 11 * cols / 20, 9 * cols / 20,
          10 * cols / 20, 11 * cols / 20], dtype=np.uint32)
-
     hand_rect_two_x = hand_rect_one_x + 10
     hand_rect_two_y = hand_rect_one_y + 10
 
-    # interates over arrays and draws rectangle on frame
+    # interates over arrays and draws rectangle on the frame
     for i in range(size):
         cv2.rectangle(frame, (hand_rect_one_y[i], hand_rect_one_x[i]),
                       (hand_rect_two_y[i], hand_rect_two_x[i]),
@@ -46,45 +43,48 @@ def hand_histogram(frame):
     global hand_rect_one_x, hand_rect_one_y
 
     # transforms input frame to HSV
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # create an image of size 90 * 10 with 3 color channels
-    roi = np.zeros([90, 10, 3], dtype=hsv_frame.dtype)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    for i in range(total_rectangle):
-        roi[i * 10: i * 10 + 10, 0: 10] = hsv_frame[hand_rect_one_x[i]:hand_rect_one_x[i] + 10,
+    # create an image of size 90 * 10 with 3 color channels
+    roi = np.zeros([90, 10, 3], dtype=hsv.dtype)
+
+    for i in range(size):
+        roi[i * 10: i * 10 + 10, 0: 10] = hsv[hand_rect_one_x[i]:hand_rect_one_x[i] + 10,
                                                     hand_rect_one_y[i]:hand_rect_one_y[i] + 10]
     
     # create a histogram using the roi matrix for the skin color
     hand_hist = cv2.calcHist([roi], [0, 1], None, [180, 256], [0, 180, 0, 256])
     return cv2.normalize(hand_hist, hand_hist, 0, 255, cv2.NORM_MINMAX)
 
-# find components of the frame that contains skin
-# use histogram to separate features in an image with back projection
+# find components of the frame that contains skin with back projection
 def hist_masking(frame, hist):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # apply back projection
+
+    # apply back projection useing a histogram to separate features in an image
     dst = cv2.calcBackProject([hsv], [0, 1], hist, [0, 180, 0, 256], 1)
+
     # smoothen the image
-    disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (33, 33))  # TODO
+    disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (31, 31))
     cv2.filter2D(dst, -1, disc, dst)
+    ret, thresh = cv2.threshold(dst, 100, 255, 0)
 
-    ret, thresh = cv2.threshold(dst, 100, 255, 0)  # TODO
     thresh = cv2.merge((thresh, thresh, thresh))
-
     cv2.GaussianBlur(dst, (3, 3), 0, dst)
 
+    # mask the input frame
     res = cv2.bitwise_and(frame, thresh)
     return res
 
-
+# find all the contours of regions of skin color
 def contours(hist_mask_image):
     gray = cv2.cvtColor(hist_mask_image, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(gray, 0, 255, 0)
+    # return a tree structure of contours
     _, cont, hierarchy = cv2.findContours(
         thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     return cont
 
-
+# find the contours with max area
 def max_contour(contour_list, frame):
     max_i = 0
     max_area = 0
@@ -96,9 +96,9 @@ def max_contour(contour_list, frame):
             max_area = area
             max_i = i
 
-    return contour_list[max_i]
+    return contour_list[max_i], max_i
 
-
+# calculate the centroid of max_contour
 def centroid(max_contour):
     moment = cv2.moments(max_contour)
     if moment['m00'] != 0:
@@ -108,7 +108,7 @@ def centroid(max_contour):
     else:
         return None
 
-
+# find the farthest point from centroid
 def farthest_point(defects, contour, centroid):
     if defects is not None and centroid is not None:
         s = defects[:, 0][:, 0]
@@ -130,70 +130,28 @@ def farthest_point(defects, contour, centroid):
         else:
             return None
 
-
-def draw_circles(frame, traverse_point):
-    if traverse_point is not None:
-        for i in range(len(traverse_point)):
-            cv2.circle(frame, traverse_point[i], int(
-                5 - (5 * i * 3) / 100), [0, 255, 255], -1)
-
-
 def manage_image_opr(frame, hand_hist):
     hist_mask_image = hist_masking(frame, hand_hist)
     contour_list = contours(hist_mask_image)
-    cv2.drawContours(frame, contour_list, -1, [255, 0, 0], 4)
-    max_cont = max_contour(contour_list, frame)
-    cv2.drawContours(frame, max_cont, 0, [0, 0, 255], 4)
+    max_cont, max_cont_i = max_contour(contour_list, frame)
+    cv2.drawContours(frame, contour_list, max_cont_i, [0, 0, 255], 4)
 
     # fitting an ellipse / rectangle
     #ellipse = cv2.fitEllipse(max_cont)
     #cv2.ellipse(frame, ellipse, (0, 255, 0),2)
-    print(max_cont)
+    
     for c in max_cont:
         M = cv2.moments(c)
         if M['m00'] != 0:
-            print("here")
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
             cv2.circle(frame, (cx, cy), 5, [255, 0, 0], -1)
 
     cnt_centroid = centroid(max_cont)
     cv2.circle(frame, cnt_centroid, 5, [255, 0, 255], -1)
-
-    if max_cont is not None:
-        hull = cv2.convexHull(max_cont, returnPoints=False)
-        defects = cv2.convexityDefects(max_cont, hull)
-        far_point = farthest_point(defects, max_cont, cnt_centroid)
-        print("Centroid : " + str(cnt_centroid) +
-              ", farthest Point : " + str(far_point))
-        cv2.circle(frame, far_point, 5, [0, 0, 255], -1)
-        if len(traverse_point) < 20:
-            traverse_point.append(far_point)
-        else:
-            traverse_point.pop(0)
-            traverse_point.append(far_point)
-
-        draw_circles(frame, traverse_point)
-
     return frame
 
 def main():
-    # Background seperation
-    parser = argparse.ArgumentParser(description='This program shows how to use background subtraction methods provided by \
-                                              OpenCV. You can process both videos and images.')
-    parser.add_argument('--input', type=str,
-                        help='Path to a video or a sequence of image.', default='vtest.avi')
-    parser.add_argument('--algo', type=str,
-                        help='Background subtraction method (KNN, MOG2).', default='MOG2')
-    args = parser.parse_args()
-
-    # create background subtractor objects
-    if args.algo == 'MOG2':
-        backSub = cv2.createBackgroundSubtractorMOG2()
-    else:
-        backSub = cv2.createBackgroundSubtractorKNN()
-
-    # Skin detection
     global hand_hist
     is_hand_hist_created = False
     capture = cv2.VideoCapture(0)
@@ -201,10 +159,8 @@ def main():
     while capture.isOpened():
         pressed_key = cv2.waitKey(1)
         _, frame = capture.read()
-        hist = frame
 
-        mask = backSub.apply(frame)
-
+        # press 'z' to take sample
         if pressed_key & 0xFF == ord('z'):
             is_hand_hist_created = True
             hand_hist = hand_histogram(frame)
@@ -212,20 +168,17 @@ def main():
         if is_hand_hist_created:
             #frame = hist_masking(frame, hand_hist)
             frame = manage_image_opr(frame, hand_hist)
-            #draw_final(frame, hand_hist)
 
         else:
             frame = draw_rect(frame)
 
         cv2.imshow("webcam", rescale_frame(frame))
-        #cv2.imshow("histogram mask", rescale_frame(hist))
 
         if pressed_key == 27:
             break
 
     cv2.destroyAllWindows()
     capture.release()
-
 
 if __name__ == '__main__':
     main()
