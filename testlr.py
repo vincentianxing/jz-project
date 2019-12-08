acb2ce5e9752c3b98e9027acbdff3595790e9137
unknown <liurantina@outlook.com>
2019-12-08 00:16:27 -0500

    test pygame

--- testlr.py
+++ testlr.py
@@ -8,42 +8,31 @@ import jianzi as jz
 import pygame
 from pygame.locals import *
 
-
-# initialize global variables for tracking
-hand_hist = None  # histogram generated from hand sample
-size = 9  # number of rectangles
-hand_rect_one_x = None
-hand_rect_one_y = None
-hand_rect_two_x = None
-hand_rect_two_y = None
-traverse_point = []
-k = True
-max_cnt = None
-
-# initialize size and global variables for ball
-WIDTH = 1000
-HEIGHT = 750
-ball_vel = [0, 0]
-ball_x = 0
-score = 0
-dist = -1
-rec = None
-image = None
-img_radius = 0
-
-# initialize screen and bounding box coordinates
-initBB = None
-screen = None
-cam = None
-
 class TestStringMethods(unittest.TestCase):
     jz.screen_init()
-    # print(pygame.display.get_caption())
-    # print(pygame.display.get_active())
 
+    # check if the screen active
     def test_screen(self):
         self.assertEqual(pygame.display.get_caption()[0], "Play Balls!")
         self.assertTrue(pygame.display.get_active())
+    
+    # check if the image load right
+    def test_img(self):
+        self.assertNotEqual(jz.image, None)
+        self.assertNotEqual(jz.rec, None)
+        self.assertEqual(jz.img_radius, 35)
+    
+    jz.init()
+
+    def test_score(self):
+        self.assertEqual(jz.score, 0)
+        self.assertEqual(jz.rec.centery, 1)
+    
+    jz.update()
+
+    def test_update(self):
+        self.assertEqual(jz.score, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
